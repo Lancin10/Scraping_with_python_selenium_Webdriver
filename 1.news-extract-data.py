11414dd24979f5747d39fe713424fd34d8a073d7
Lancine Sérifou <42044048+Lancin10@users.noreply.github.com>
2022-08-10 15:54:21 +0200

    Update 1.news-extract-data.py

--- 1.news-extract-data.py
+++ 1.news-extract-data.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 import pandas as pd
 
 web = 'https://www.thesun.co.uk/sport/football/'
-path = '/Users/frankandrade/Downloads/chromedriver'  # introduce path here
+path = '/Users/zahira/Desktop/WEB SCRPAING USING PYTHON/Scraping_automation_with_python/chromedriver'  # introduce path here
 
 # Creating the driver
 driver_service = Service(executable_path=path)
